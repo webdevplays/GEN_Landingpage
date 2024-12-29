097b51252e26dc43b7470f9a29b01bc616fa712f
WEB_APIDeveloper <elthrone1233@gmail.com>
2024-12-29 16:27:01 +0800

    Update index-9e9aa9ae21ae59ed.js

--- _next/static/chunks/pages/index-9e9aa9ae21ae59ed.js
+++ _next/static/chunks/pages/index-9e9aa9ae21ae59ed.js
@@ -495,7 +495,7 @@
                     duration: 0.75,
                     delay: 250,
                     children: (0, t.jsx)(m.A, {
-                      url: "",
+                      url: "https://t.me/",
                       children: (0, t.jsxs)(t.Fragment, {
                         children: [
                           (0, t.jsx)("span", { children: "JOIN COMMUNITY" }),
