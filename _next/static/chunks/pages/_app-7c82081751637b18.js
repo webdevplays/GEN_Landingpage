7e9b8a6900a2aa8ee5ed07f58709f39a3ddf24a5
WEB_APIDeveloper <elthrone1233@gmail.com>
2025-01-11 22:52:10 +0800

    commit

--- _next/static/chunks/pages/_app-7c82081751637b18.js
+++ _next/static/chunks/pages/_app-7c82081751637b18.js
@@ -1915,7 +1915,7 @@
         f = () =>
           (0, n.jsx)("a", {
             className: l().social,
-            href: "https://x.com/Gen_Ai25",
+            href: "https://x.com/genedotfun",
             target: "_blank",
             rel: "noreferrer",
             children: (0, n.jsx)("svg", {
